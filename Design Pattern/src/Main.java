3f079de049b1f25097556770b1a5e3fe9ae6ec93
binod.yadav <binod.yadav@braneenterprises.com>
2024-07-11 13:46:57 +0530

    design pattern learnings

---
+++ Design Pattern/src/Main.java
@@ -0,0 +1,11 @@
+import SingletonPattern.Dog;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Dog dog = new Dog();
+        dog.getDogName();
+        System.out.println("Hello world!");
+    }
+}
---
+++ Design Pattern/src/SingletonPattern/Dog.java
@@ -0,0 +1,8 @@
+package SingletonPattern;
+
+public class Dog {
+    public void getDogName()
+    {
+        System.out.println("inside dog class");
+    }
+}

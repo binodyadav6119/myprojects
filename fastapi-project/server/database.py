09681f79d1116186a221b903f00e0478051f9964
BINOD YADAV <binodyadav6119@gmail.com>
2024-08-26 14:26:15 +0530

    shopify poc

--- fastapi-project/server/database.py
+++ fastapi-project/server/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 # from sqlalchemy
 
-DATABASE_URL = "postgresql://avnadmin:AVNS_HmxRJkpLOIDUVvoBSrV@binod-pg-binodmnnit2017-b27e.l.aivencloud.com:24037/defaultdb?sslmode=require"
+DATABASE_URL = ""
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
